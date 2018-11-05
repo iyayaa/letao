9d2ff16ebd7ab82c7ec9818efcc6d2c0e57fb84a
ilin chang <niliyzhang@foxmail.com>
2018-11-05 15:57:13 +0800

    '后台系统登录，验证'

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,84 +1,73 @@
 $(function(){
-    /*前端校验功能  bootstrap validator*/
-    /*1.完整的表单结构  form   input  submit 这些元素*/
-    /*2.表单元素需要对应的名字 name="username" */
-    /*3.初始化表单验证组件 插件*/
-    /*4.配置组件功能*/
-    /*5.配置具体的属性需要的校验规则*/
-    $('#login').bootstrapValidator({
-        /*提示的图标*/
+	$('#login').bootstrapValidator({
+		/*配置校验的不同状态下显示的图标*/
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
             validating: 'glyphicon glyphicon-refresh'
         },
-        /*属性对应的是表单元素的名字*/
-        fields:{
-            /*配置校验规则*/
-            username:{
-                /*规则*/
+        /*需要校验的表单元素 通过名称 name*/
+        fields: {
+        	/*对应表单元素的name*/
+            username: {
+                /*校验规则 多个校验规则*/
                 validators: {
                     notEmpty: {
-                        message: '用户名不能为空'
+                        message: '请输入用户名'
                     },
-                    /*设置错误信息 和规则无关 和后台校验有关系*/
-                    callback: {
-                        message: '用户名错误'
+                    /*配置一个校验规则*/
+                    callback:{
+                        message:'用户名不存在'
                     }
                 }
             },
-            password:{
-                validators:{
+            password: {
+                validators: {
                     notEmpty: {
-                        message: '密码不能为空'
+                        message: '请输入密码'
                     },
-                    stringLength:{
-                        min:6,
-                        max:18,
-                        message:'密码在6-18个字符内'
+                    stringLength: {
+                        min: 6,
+                        max: 18,
+                        message: '密码必须是6-18个字符'
                     },
-                    callback: {
-                        message: '密码不正确'
+                    callback:{
+                        message:'密码错误'
                     }
                 }
             }
         }
-        /*7.表单校验成功*/
-    }).on('success.form.bv', function(e) {
-        /*禁用默认提交的事件 因为要使用ajax提交而不是默认的提交方式*/
-        e.preventDefault();
-        /*获取当前的表单*/
+	}).on('success.form.bv', function (e) {
+		e.preventDefault();
         var $form = $(e.target);
-        /*发送登录请求*/
-        $.ajax({
-            type:'post',
-            url:'/employee/employeeLogin',
-            data:$form.serialize(),
-            dataType:'json',
-            success:function(data){
-                if(data.success){
-                    /*后台管理员 root 123456*/
-                    /*登录成功*/
-                    location.href = 'index.html';
-                }else{
-                    /*登录不成功*/
-                    /*8.恢复可提交的按钮*/
-                    $form.data('bootstrapValidator').disableSubmitButtons(false);
-                    /*9.指定某一个表单元素的错误提示*/
-                    /* NOT_VALIDATED, VALIDATING, INVALID or VALID */
+		// console.log($(e.target).serialize());
+		// return;
+		$.ajax({
+			url:'/employee/employeeLogin',
+			type:'post',
+			data:$form.serialize(),
+			dataType:'json',
+			success:function (data) {
+			    /*业务成功*/
+                if(data.success == true){
+                    location.href = '/admin/';
+                }
+                /*业务失败*/
+                else {
                     if(data.error == 1000){
+                        /*用户名错误*/
                         $form.data('bootstrapValidator').updateStatus('username','INVALID','callback');
                     }else if(data.error == 1001){
+                        /*密码错误*/
                         $form.data('bootstrapValidator').updateStatus('password','INVALID','callback');
                     }
                 }
+			},
+            error:function(data){
+               
             }
-        });
-    });
-    /*重置功能*/
-    $('[type="reset"]').on('click',function(){
-        /*6.重置验证*/
-        $('#login').data('bootstrapValidator').resetForm();
-    });
+		});
+
+	});
 
 });
